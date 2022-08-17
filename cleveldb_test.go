96d57515fdaa51ad14eaf06871710bbab79362c1
Jacob Gadikian <jacobgadikian@gmail.com>
2022-08-18 06:28:47 +0700

    Update cleveldb_test.go

--- cleveldb_test.go
+++ cleveldb_test.go
@@ -25,7 +25,6 @@ func TestWithClevelDB(t *testing.T) {
 	t.Run("ClevelDB", func(t *testing.T) { Run(t, db) })
 }
 
-// nolint: errcheck
 func BenchmarkRandomReadsWrites2(b *testing.B) {
 	b.StopTimer()
 
@@ -51,10 +50,14 @@ func BenchmarkRandomReadsWrites2(b *testing.B) {
 			val := internal[idx]
 			idxBytes := int642Bytes(idx)
 			valBytes := int642Bytes(val)
-			db.Set(
+			err := db.Set(
 				idxBytes,
 				valBytes,
 			)
+			if err != nil {
+			fmt.Println("couldn't write something: cleveldb_test.go line 53")
+			}
+
 		}
 		// Read something
 		{

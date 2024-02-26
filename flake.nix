fab0ae047fcf9a7529715f17d400fa3268a370e3
Julien Robert <julien@rbrt.fr>
2024-02-26 10:56:18 +0100

    build: downgrade go to 1.19 (#102)
    * build: downgrade go to 1.19
    
    * updates
    
    * updates

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
           devShells = rec {
             default = with pkgs; mkShell {
               buildInputs = [
-                go_1_22 # Use Go 1.22 version
+                go_1_20 # Use Go 1.20 version
                 rocksdb
               ];
             };
